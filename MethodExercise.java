5eb1c9603bea6e10bfe92721ffa2c797f848d78c
Jamie Gable <146774360+jamiegable24@users.noreply.github.com>
2023-11-04 16:53:07 -0500

    [Exercise] Loop Until Valid Input is Received
    Program runs but doesn't terminate

--- MethodExercise.java
+++ MethodExercise.java
@@ -11,9 +11,18 @@ public class MethodExercise {
 		Integer getNumber(Integer convertedInput) {
 		if (convertedInput >= 50 && convertedInput <= 300) {
 			this.convertedInput = convertedInput;
-			return convertedInput;			
-			} else {
-				return null;
+			return convertedInput;		
+			} else {		
+					
+					int i = 0;
+					while (convertedInput < 50 || convertedInput > 300) {
+						System.out.println("Oops, that number wasn't between "
+								+ "50 and 300, try again: ");
+						i = i +1;
+					}
+					System.out.println("Done");
+				
+				return convertedInput;
 			}	
 		}		
 }
--- Week2Example.java
+++ Week2Example.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Week2Example {
 
 	public static void main (String[] args) {
-		
-		
+		Integer convertedInput = 1;	
 		
 		MethodExercise input = new MethodExercise();
-		
+				
 		input.getNumber(convertedInput);
 		System.out.println("The number you typed in was: " + convertedInput);
 	}
